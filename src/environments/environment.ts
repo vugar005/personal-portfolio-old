956228a1aa20fb9fc37f85e194ad113992eff82f
Vugar <abdullayev1.618@hotmail.com>
2021-12-11 23:36:56 +0100

    add firebase to project (#1)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    projectId: 'personal-portfolio-cddec',
+    appId: '1:907083366382:web:c69f2ba13c0495e5aed8ff',
+    storageBucket: 'personal-portfolio-cddec.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAr92OWpapQggU1TJPs-5KSrYNxuhR9zmI',
+    authDomain: 'personal-portfolio-cddec.firebaseapp.com',
+    messagingSenderId: '907083366382',
+    measurementId: 'G-GTV89S20BZ',
+  }
 };
 
 /*
